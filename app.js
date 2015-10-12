d257edceeb717238b8a552d358cfce5ffd1dd839
Roland D. San Nicolas <rdsannicolas@gmail.com>
2015-10-12 12:49:21 -0700

    Initial Push adding files and directory.

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+/*
+JavaScript:
+Photo constructor
+path to the photo at img\
+total votes received by photo
+name for the photo "Bright Red Power Drill"
+
+Tracker Object
+Know where the photos are (hint: array of photo instances)
+method to display 2 random photos
+method to receive and tally votes/ratings that are then stored in the photo instances
+method to display chart of results
+*/
