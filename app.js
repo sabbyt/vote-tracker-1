a04ca9df1e9481e9bf1a9129f3fbc848346cd60d
Roland D. San Nicolas <rdsannicolas@gmail.com>
2015-10-13 21:19:31 -0700

    Worked on code, but still not operational.

--- app.js
+++ app.js
@@ -1,28 +1,73 @@
-/* Pseudo code
+//array of photo instances
+var photoArray = [];
 
-var picture1
-var picture2
+var photo = function(name,path,votes){
+  this.name = name;
+  this.path = path;
+  this.votes = 0;
+}
 
-listen for event click
+//method to display 2 random photos
+var random = function() {
+  return Math.floor(Math.random();
+};
 
-  if picture1 is chosen
-    increase picture 1 count by 1
-  if picture 2 is chosen
-    increase picture 2 count by 2
-
-Display separate page with chart with how many times the picture was chosen.
-
+}
+/*
+//event listener per click
+var picButton = document.getElementById('picButton');
+picButton.addEventListener('click', addImages);
+picButton.addEventListener('click', function(){
+addImages();
+console.log("Some answer");
+});
 */
+
+
+
+//tally votes per photo
+// Tracker Object
+  //method to receive and tally votes/ratings that are then stored in the photo instances
+  //method to display chart of results
+/*
+  tracker.method = function() {
+  does this thing
+  does this other little thing
+};
+tracker.method 2 = function() {
+  does something else
+};
+
+tracker.displayImages();
+*/
+
+
+
+
+var puppy1 = new puppies('Puppy 1', 'images/pic1.jpeg', 0);
+var puppy2 = new puppies('Puppy 2','images/pic2.jpeg', 0);
+var puppy3 = new puppies('Puppy 3','images/pic3.jpeg', 0);
+var puppy4 = new puppies('Puppy 4','images/pic4.jpeg', 0);
+var puppy5 = new puppies('Puppy 5','images/pic5.jpeg', 0);
+var puppy6 = new puppies('Puppy 6','images/pic6.jpeg', 0);
+var puppy7 = new puppies('Puppy 7','images/pic7.jpeg', 0);
+var puppy8 = new puppies('Puppy 8','images/pic8.jpeg', 0);
+var puppy9 = new puppies('Puppy 9','images/pic9.jpeg', 0);
+var puppy10 = new puppies('Puppy 10','images/pic10.jpeg', 0);
+var puppy11 = new puppies('Puppy 11','images/pic11.jpeg', 0);
+var puppy12 = new puppies('Puppy 12','images/pic12.jpeg', 0);
+
 
 /*
-Photo constructor
-path to the photo at img\
-total votes received by photo
-name for the photo "Bright Red Power Drill"
+statements to test static images to html
+var puppy1 = document.getElementById('option1');
+var img1 = document.createElement('img');
+img1.src = 'images/pic1.jpeg';
+puppy1.appendChild(img1);
 
-Tracker Object
-Know where the photos are (hint: array of photo instances)
-method to display 2 random photos
-method to receive and tally votes/ratings that are then stored in the photo instances
-method to display chart of results
+var puppy2 = document.getElementById('option2');
+var img2 = document.createElement('img');
+img2.src = 'images/pic2.jpeg';
+puppy2.appendChild(img2);
 */
+
